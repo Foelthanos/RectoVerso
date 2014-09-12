a031c2bd30aee725fb9f53d5f0bb10e32518a6eb
NeXibaf <NeXibaf@outlook.com>
2014-09-12 15:03:02 +0200

    Level select - initialisation
    Il y a encore les icones de selection de niveau à faire

---
+++ Game/core/src/com/rectoverso/model/Level.java
@@ -0,0 +1,49 @@
+package com.rectoverso.model;
+
+public class Level {
+
+	public enum LevelState {
+		PRINTED,LOADED
+	};
+	public enum LevelType {
+		normal,secret,movie
+	};
+	
+	private LevelState state = LevelState.PRINTED;
+	private int number;
+	private LevelType type;
+	private String name;
+	private boolean locked = true;
+	
+	public Level(int number, String name , LevelType type){
+		this.number = number;
+		this.name = name;
+		this.type = type;
+		
+	}
+	public void setLocked(boolean lock){
+		if(this.locked && !lock){
+			unlock();
+		}
+		else if (!this.locked && lock){
+			lock();
+		}
+		
+	}
+	private void unlock(){
+		this.locked  = false;
+	}
+	private void lock(){
+		this.locked = true;
+	}
+
+	public String getName(){
+		return name;
+	}
+	public int getNumber(){
+		return number;
+	}
+	public LevelType getType(){
+		return type;
+	}
+}
---
+++ Game/core/src/com/rectoverso/utils/LevelIconButton.java
@@ -0,0 +1,5 @@
+package com.rectoverso.utils;
+
+public class LevelIconButton {
+
+}
